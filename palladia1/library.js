f9d244fe32157850142945f5237c3afdfdc17664
Hemant-Rajawat1412 <166383411+Hemant-Rajawat1412@users.noreply.github.com>
2024-04-22 22:40:50 +0530

    Palladian Designer's webpage

---
+++ palladia1/library.js
@@ -0,0 +1,18 @@
+var navLinks = document.getElementById("navLinks");
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+
+var navBar = document.getElementsById("nav_Links");
+
+function toggle() {
+    nav_Links.style.right = '0';
+}
+
+function hideToggle() {
+    nav_Links.style.right = "-200px";
+}
+
